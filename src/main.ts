5c891f850b86a679c4f6edb1bf2a656adec31599
Oleksandr Komnatskyi <akomnatskyi@gmail.com>
2025-06-28 14:03:23 +0300

    task 3 is completed

--- src/main.ts
+++ src/main.ts
@@ -74,16 +74,32 @@ console.log(newUser)
  *
  */
 
-enum OrderStatus {}
+enum OrderStatus {
+  Pending = 'Pending',
+  Shipped = 'Shipped',
+  Delivered = 'Delivered',
+  Cancelled = 'Cancelled'
+}
 
-function getOrderStatus() {
-  // code here
+function getOrderStatus(status: OrderStatus): string {
+  switch (status) {
+    case OrderStatus.Pending:
+      return 'Замовлення очікує на обробку';
+    case OrderStatus.Shipped:
+      return 'Замовлення було відправлено';
+    case OrderStatus.Delivered:
+      return 'Замовлення доставлено';
+    case OrderStatus.Cancelled:
+      return 'Замовлення скасовано';
+    default:
+      throw new Error('Невідомий статус замовлення');
+  }
 }
 
 // Приклад виклику функції
-// console.log(getOrderStatus(OrderStatus.Pending))
-// console.log(getOrderStatus(OrderStatus.Shipped))
-// console.log(getOrderStatus(OrderStatus.Delivered))
-// console.log(getOrderStatus(OrderStatus.Cancelled))
+console.log(getOrderStatus(OrderStatus.Pending))
+console.log(getOrderStatus(OrderStatus.Shipped))
+console.log(getOrderStatus(OrderStatus.Delivered))
+console.log(getOrderStatus(OrderStatus.Cancelled))
 
-// export { sumArray, createUser, OrderStatus, getOrderStatus }
+export { sumArray, createUser, OrderStatus, getOrderStatus }
